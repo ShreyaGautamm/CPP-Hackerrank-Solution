b3f9d4149e473b39914ae3076ee882da87eaa593
Shreya Gautam <gautamm.shreya@gmail.com>
2021-02-28 18:19:06 +0530

    Create ArrayIntroduction.cpp

---
+++ Introduction/ArrayIntroduction.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+    for(int i= n-1; i>=0; i--)
+    { 
+        cout<<a[i]<<" "; 
+    }
+    
+    return 0;
+}
